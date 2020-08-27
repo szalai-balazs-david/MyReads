8b10d68e164f5ab68fe5d188607b2e2c3d66b188
Balazs <balazs@altaeros.com>
2020-08-26 20:55:01 -0400

    Add option to deal with books without image

--- src/Book.js
+++ src/Book.js
@@ -5,7 +5,9 @@ function Book(props){
   return (
     <div className="book">
       <div className="book-top">
-        <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url("${book.imageLinks.thumbnail}")` }}></div>
+        {book.imageLinks 
+        ? <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url("${book.imageLinks.thumbnail}")` }}></div>
+        : <div className="book-cover" style={{ width: 128, height: 193 }}></div>}
         <div className="book-shelf-changer">
           <select value={shelf} onChange={(event) => onShelfChange(book, event.target.value)}>
             <option value='move' disabled>Move to...</option>
